e8c8b27a48bd55d7bff86afcd3ffef8733e4ef83
David Krmpotic <david.krmpotic@gmail.com>
2016-03-20 00:09:08 +0100

    move code template to frontend folder

--- cute-files.js
+++ cute-files.js
@@ -40,7 +40,7 @@ app.use('/', express.static(path.join(__dirname, 'frontend')));
 
 // Serve files from the current directory under the /files route
 
-var template = Handlebars.compile(fs.readFileSync(path.join(__dirname, '/templates/code.html')).toString());
+var template = Handlebars.compile(fs.readFileSync(path.join(__dirname, '/frontend/code.handlebars.html')).toString());
 
 app.use('/files', function(req, res) {
   res.send(template({ code: fs.readFileSync(path.join(__dirname, req.path)).toString() }))
